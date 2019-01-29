d698703c16662f874c801ee07109869c2450a57c
nitrog42 <nitrog42@gmail.com>
2019-01-29 10:20:44 +0100

    Added imagemagick with rsvg formula

---
+++ Formula/imagemagick-librsvg.rb
@@ -0,0 +1,75 @@
+class Imagemagick < Formula
+    desc "Tools and libraries to manipulate images in many formats"
+    homepage "https://www.imagemagick.org/"
+    # Please always keep the Homebrew mirror as the primary URL as the
+    # ImageMagick site removes tarballs regularly which means we get issues
+    # unnecessarily and older versions of the formula are broken.
+    url "https://dl.bintray.com/homebrew/mirror/ImageMagick-7.0.8-24.tar.xz"
+    mirror "https://www.imagemagick.org/download/ImageMagick-7.0.8-24.tar.xz"
+    sha256 "a547dc24fede9d7340ca96b4b68c14071c302b237bf348c8e0ec541cf75b11a0"
+    head "https://github.com/ImageMagick/ImageMagick.git"
+  
+    bottle do
+      sha256 "3233f90e1fe6d713fa74b019c295576503583e44ab7b4d1d9cd835078ef859db" => :mojave
+      sha256 "4be13792e97fa6d33a3be33c233376f04c5e478c60a16d0d268ab363622934fa" => :high_sierra
+      sha256 "7075a6cf60480227b9a726b9dbe2d952cc5d66843679f9629bd87bbe1b8f3bd3" => :sierra
+    end
+  
+    depends_on "pkg-config" => :build
+  
+    depends_on "freetype"
+    depends_on "jpeg"
+    depends_on "libomp"
+    depends_on "libpng"
+    depends_on "librsvg"
+    depends_on "libtiff"
+    depends_on "libtool"
+    depends_on "little-cms2"
+    depends_on "openjpeg"
+    depends_on "webp"
+    depends_on "xz"
+  
+    skip_clean :la
+  
+    def install
+      args = %W[
+        --disable-osx-universal-binary
+        --prefix=#{prefix}
+        --disable-dependency-tracking
+        --disable-silent-rules
+        --disable-opencl
+        --enable-shared
+        --enable-static
+        --with-freetype=yes
+        --with-modules
+        --with-openjp2
+        --with-rsvg
+        --with-webp=yes
+        --without-gslib
+        --with-gs-font-dir=#{HOMEBREW_PREFIX}/share/ghostscript/fonts
+        --without-fftw
+        --without-pango
+        --without-x
+        --without-wmf
+        --enable-openmp
+        ac_cv_prog_c_openmp=-Xpreprocessor\ -fopenmp
+        ac_cv_prog_cxx_openmp=-Xpreprocessor\ -fopenmp
+        LDFLAGS=-lomp
+      ]
+  
+      # versioned stuff in main tree is pointless for us
+      inreplace "configure", "${PACKAGE_NAME}-${PACKAGE_VERSION}", "${PACKAGE_NAME}"
+      system "./configure", *args
+      system "make", "install"
+    end
+  
+    test do
+      assert_match "PNG", shell_output("#{bin}/identify #{test_fixtures("test.png")}")
+      # Check support for recommended features and delegates.
+      features = shell_output("#{bin}/convert -version")
+      %w[Modules freetype jpeg png tiff].each do |feature|
+        assert_match feature, features
+      end
+    end
+  end
+  
