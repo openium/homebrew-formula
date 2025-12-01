25eef146151ff7556568df65a5f600d6ce30e020
Ludo "Skyle" Káyma <17049216+SkyleKayma@users.noreply.github.com>
2025-12-01 16:30:12 +0100

    Updated to last formula (7.1.2-9)

--- Formula/imagemagick-librsvg.rb
+++ Formula/imagemagick-librsvg.rb
@@ -1,90 +1,115 @@
 class ImagemagickLibrsvg < Formula
   desc "Tools and libraries to manipulate images in many formats"
   homepage "https://imagemagick.org/index.php"
-  url "https://imagemagick.org/archive/releases/ImageMagick-7.1.1-16.tar.xz"
-  sha256 "059a51a9f61000a82a2d2974372eddbec9ce3d420c7493187388c1b0d49b90c0"
+  url "https://imagemagick.org/archive/releases/ImageMagick-7.1.2-9.tar.xz"
+  sha256 "f1c14b41a1a48762db63b1db4eddbeb251d8dfebdc13d214282e15bbbfc2bc34"
   license "ImageMagick"
   head "https://github.com/ImageMagick/ImageMagick.git", branch: "main"
-  
-    livecheck do
-      url "https://imagemagick.org/archive/"
-      regex(/href=.*?ImageMagick[._-]v?(\d+(?:\.\d+)+-\d+)\.t/i)
-    end
-  
-    depends_on "pkg-config" => :build
-    depends_on "freetype"
-    depends_on "ghostscript"
-    depends_on "jpeg-turbo"
-    depends_on "libheif"
-    depends_on "liblqr"
-    depends_on "libpng"
-    depends_on "libraw"
-    depends_on "librsvg"
-    depends_on "libtiff"
-    depends_on "libtool"
-    depends_on "little-cms2"
-    depends_on "openexr"
-    depends_on "openjpeg"
-    depends_on "webp"
-    depends_on "xz"
-  
-    uses_from_macos "bzip2"
-    uses_from_macos "libxml2"
-    uses_from_macos "zlib"
 
-    on_macos do
-      depends_on "libomp"
-    end
+  livecheck do
+    url "https://imagemagick.org/archive/"
+    regex(/href=.*?ImageMagick[._-]v?(\d+(?:\.\d+)+-\d+)\.t/i)
+  end
+
+  bottle do
+    sha256 arm64_tahoe:   "c8e11b70bef606402250ced23ac824ad43a4fdcf6dbd4586f2116eb2794fa9b0"
+    sha256 arm64_sequoia: "a8d237fb8cf555785013eb20e35d21e84651afc0a77b948f3a490888068b9c64"
+    sha256 arm64_sonoma:  "385933d3ca71714350fef384ce25974e47b47eb4dc74be32a495b8127177ccf0"
+    sha256 sonoma:        "ae84d25de26ae5c8db4e21b64428c31f216f5315b1463a5ebb8a0d416fb1a55d"
+    sha256 arm64_linux:   "12c85837ff3f966b43d308ce0d8ee964994cfcf6703952b74a230d404c31d1a6"
+    sha256 x86_64_linux:  "db4eb462fe6e61b310344fdf5f109711b180a67f4fdb9e160380444cfba02849"
+  end
   
-    on_linux do
-      depends_on "libx11"
-    end
+  depends_on "pkgconf" => :build
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "jpeg-turbo"
+  depends_on "jpeg-xl"
+  depends_on "libheif"
+  depends_on "liblqr"
+  depends_on "libpng"
+  depends_on "libraw"
+  depends_on "librsvg"
+  depends_on "libtiff"
+  depends_on "libtool"
+  depends_on "libultrahdr"
+  depends_on "libzip"
+  depends_on "little-cms2"
+  depends_on "openexr"
+  depends_on "openjpeg"
+  depends_on "webp"
+  depends_on "xz"
   
-    skip_clean :la
+  uses_from_macos "bzip2"
+  uses_from_macos "libxml2"
+  uses_from_macos "zlib"
+
+  on_macos do
+    depends_on "gettext"
+    depends_on "glib"
+    depends_on "imath"
+    depends_on "libomp"
+  end
+
+  on_linux do
+    depends_on "libx11"
+    depends_on "libxext"
+  end
   
-    def install
-      # Avoid references to shim
-      inreplace Dir["**/*-config.in"], "@PKG_CONFIG@", Formula["pkg-config"].opt_bin/"pkg-config"
-      # versioned stuff in main tree is pointless for us
-      inreplace "configure", "${PACKAGE_NAME}-${PACKAGE_BASE_VERSION}", "${PACKAGE_NAME}"
+  skip_clean :la
   
-      args = [
-        "--enable-osx-universal-binary=no",
-        "--disable-silent-rules",
-        "--disable-opencl",
-        "--enable-shared",
-        "--enable-static",
-        "--with-freetype=yes",
-        "--with-gvc=no",
-        "--with-modules",
-        "--with-openjp2",
-        "--with-openexr",
-        "--with-rsvg",
-        "--with-webp=yes",
-        "--with-heic=yes",
-        "--with-raw=yes",
-        "--with-gslib",
-        "--with-gs-font-dir=#{HOMEBREW_PREFIX}/share/ghostscript/fonts",
-        "--with-lqr",
-        "--without-djvu",
-        "--without-fftw",
-        "--without-pango",
-        "--without-wmf",
-        "--enable-openmp",
+  def install
+    # Avoid references to shim
+    inreplace Dir["**/*-config.in"], "@PKG_CONFIG@", Formula["pkg-config"].opt_bin/"pkg-config"
+    # versioned stuff in main tree is pointless for us
+    inreplace "configure", "${PACKAGE_NAME}-${PACKAGE_BASE_VERSION}", "${PACKAGE_NAME}"
+  
+    args = [
+      "--enable-osx-universal-binary=no",
+      "--disable-silent-rules",
+      "--disable-opencl",
+      "--enable-shared",
+      "--enable-static",
+      "--with-freetype=yes",
+      "--with-gvc=no",
+      "--with-modules",
+      "--with-openjp2",
+      "--with-openexr",
+      "--with-rsvg",
+      "--with-webp=yes",
+      "--with-heic=yes",
+      "--with-raw=yes",
+      "--with-uhdr=yes",
+      "--with-zip=yes",
+      "--without-gslib",
+      "--with-gs-font-dir=#{HOMEBREW_PREFIX}/share/ghostscript/fonts",
+      "--with-lqr",
+      "--without-djvu",
+      "--without-fftw",
+      "--without-pango",
+      "--without-wmf",
+      "--enable-openmp",
+    ]
+    if OS.mac?
+      args += [
+        "--without-x",
+        # Work around "checking for clang option to support OpenMP... unsupported"
+        "ac_cv_prog_c_openmp=-Xpreprocessor -fopenmp",
+        "ac_cv_prog_cxx_openmp=-Xpreprocessor -fopenmp",
+        "LDFLAGS=-lomp -lz",
       ]
-      if OS.mac?
-        args += [
-          "--without-x",
-          # Work around "checking for clang option to support OpenMP... unsupported"
-          "ac_cv_prog_c_openmp=-Xpreprocessor -fopenmp",
-          "ac_cv_prog_cxx_openmp=-Xpreprocessor -fopenmp",
-          "LDFLAGS=-lomp -lz",
-        ]
-      end
-  
-      system "./configure", *std_configure_args, *args
-      system "make", "install"
     end
+
+    system "./configure", *std_configure_args, *args
+    system "make", "install"
+  end
+
+  def caveats
+    <<~EOS
+      Ghostscript is not installed by default as a dependency.
+      If you need PS or PDF support, ImageMagick will still use the ghostscript formula if installed directly.
+    EOS
+  end
   
     test do
       assert_match "PNG", shell_output("#{bin}/identify #{test_fixtures("test.png")}")
@@ -100,6 +125,5 @@ class ImagemagickLibrsvg < Formula
       ["AVIF  HEIC      rw+", "ARW  DNG       r--", "DNG  DNG       r--"].each do |format|
         assert_match format, formats
       end
-      assert_match "Helvetica", shell_output("#{bin}/magick -list font")
     end
   end
